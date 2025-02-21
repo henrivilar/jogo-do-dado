1561f20bd91de5d6de57346a057d775f9999208c
henrivilar <fabio.fv1@hotmail.com>
2025-02-21 18:46:25 -0300

    Project finished

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Condições de partida
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+  let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const startingPoint = function() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+startingPoint();
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,7 +44,6 @@ const switchPlayer = function() {
 };
 
 //Função girar o dado
-
 btnRoll.addEventListener('click', function() {
   if(playing) {
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -47,11 +59,12 @@ btnRoll.addEventListener('click', function() {
   }
 });
 
+//Botão para armazenar o valor atual
 btnHold.addEventListener('click', function() {
   if(playing) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -61,3 +74,6 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+//Botão com função para resetar os valores
+btnNew.addEventListener('click', startingPoint);
